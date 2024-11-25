decf335c0053e3e4b3593a5f8a50fb98eade021b
VK-Garg7 <14garg04@gmail.com>
2024-11-26 03:42:12 +0530

    navbar tailwind css

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,32 +1,60 @@
-import React from 'react'
-import { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
-
-    const [menu, setMenu] = useState("home")
+  const [menu, setMenu] = useState("home");
 
   return (
-    <div className='navbar'>
-      <img src={assets.logo} alt="" className="logo" />
-      <ul className="navbar-menu">
-        <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</li>
-        <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</li>
-        <li onClick={() => setMenu("about us")} className={menu === "about us" ? "active" : ""}>about us</li>
-        <li onClick={() => setMenu("contact us")} className={menu === "contact us" ? "active" : ""}>contact us</li>
+    <div className="py-5 flex justify-between items-center">
+      <img src={assets.logo} alt="Logo" className="w-[150px]" />
+
+      <ul className="flex gap-5 text-[#49557e] text-lg list-none">
+        <li
+          onClick={() => setMenu("home")}
+          className={`cursor-pointer pb-1 ${
+            menu === "home" ? "border-b-2 border-brown" : ""
+          }`}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={`cursor-pointer pb-1 ${
+            menu === "menu" ? "border-b-2 border-brown" : ""
+          }`}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("about us")}
+          className={`cursor-pointer pb-1 ${
+            menu === "about us" ? "border-b-2 border-brown" : ""
+          }`}
+        >
+          about us
+        </li>
+        <li
+          onClick={() => setMenu("contact us")}
+          className={`cursor-pointer pb-1 ${
+            menu === "contact us" ? "border-b-2 border-brown" : ""
+          }`}
+        >
+          contact us
+        </li>
       </ul>
 
-      <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
-        <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
+      <div className="flex items-center gap-10">
+        <img src={assets.search_icon} alt="Search Icon" />
+        <div className="relative">
+          <img src={assets.basket_icon} alt="Basket Icon" />
+          <div className="absolute top-[-8px] right-[-8px] w-[10px] h-[10px] bg-brown rounded-full"></div>
         </div>
-        <button>Sign In</button>
+        <button className="bg-transparent text-[#49557e] border border-brown py-2 px-6 rounded-full transition duration-300 hover:bg-brown hover:text-white">
+          Sign In
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
