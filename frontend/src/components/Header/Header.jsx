84ba16bd982fcd1f5329d3982485f1c83d8f9516
VK-Garg7 <14garg04@gmail.com>
2024-11-26 23:05:22 +0530

    header update

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="header-contents">
-        <h2>Order your favourite food here</h2>
-        <p>
+    <div className="h-[34vw] my-8 mx-auto bg-[url('/header_img.png')] bg-no-repeat bg-contain relative">
+      <div className="absolute flex flex-col items-start gap-[1.5vw] max-w-[50%] bottom-[10%] left-[6vw] animate-fadeIn">
+        <h2 className="font-medium text-white text-[max(4.5vw,20px)]">
+          Order your favourite food here
+        </h2>
+        <p className="text-white text-[1vw]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi officia
           illum, quibusdam earum vitae amet? Pariatur voluptates dolores a fuga!
         </p>
-        <button>View Menu</button>
+        <button className="border-none text-[#747474] font-medium px-[2.3vw] py-[1vw] bg-white text-[max(1vw,13px)] rounded-full">
+          View Menu
+        </button>
       </div>
     </div>
   );
