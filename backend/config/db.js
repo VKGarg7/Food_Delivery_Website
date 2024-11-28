eebc8b47b55f749bc85f7028db21c537ae97affe
VK-Garg7 <14garg04@gmail.com>
2024-11-29 00:30:50 +0530

    database connecting

---
+++ backend/config/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://VANSH:Vansh%23147@cluster0.3anvn.mongodb.net/Food_Delivery')
+        .then(() => console.log("Connected"))
+};
