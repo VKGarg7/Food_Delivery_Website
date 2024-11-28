e41a185f924abc4742e27ffa7192c36ae042caef
VK-Garg7 <14garg04@gmail.com>
2024-11-28 18:51:58 +0530

    css change

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,14 +1,89 @@
+// import React, { useContext } from "react";
+// import "./Cart.css";
+// import { StoreContext } from "../../context/StoreContext";
+
+// const Cart = () => {
+//   const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+
+//   return (
+//     <div className="cart">
+//       <div className="cart-items">
+//         <div className="cart-item-title">
+//           <p>Items</p>
+//           <p>Title</p>
+//           <p>Price</p>
+//           <p>Quantity</p>
+//           <p>Total</p>
+//           <p>Remove</p>
+//         </div>
+//         <br />
+//         <hr />
+//         {food_list.map((item, index) => {
+//           if (cartItems[item._id] > 0) {
+//             return (
+//               <>
+//                 <div key={index} className="cart-item-title cart-items-item">
+//                   <img src={item.image} alt="" />
+//                   <p>{item.name}</p>
+//                   <p>Rs. {item.price}</p>
+//                   <p>{cartItems[item._id]}</p>
+//                   <p>Rs. {item.price * cartItems[item._id]}</p>
+//                   <p onClick={() => removeFromCart(item._id)} className="cross">
+//                     X
+//                   </p>
+//                 </div>
+//                 <hr />
+//               </>
+//             );
+//           }
+//         })}
+//         <div className="cart-bottom">
+//           <div className="cart-total">
+//             <h2>Cart Total</h2>
+//             <div className="cart-total-details">
+//               <p>SubTotal</p>
+//               <p>Rs. {getTotalCartAmount()}</p>
+//             </div>
+//             <hr />
+//             <div className="cart-total-details">
+//               <p>Delivery Fee</p>
+//               <p>Rs. {2}</p>
+//             </div>
+//             <hr />
+//             <div className="cart-total-details">
+//               <b>Total</b>
+//               <b>Rs. {getTotalCartAmount()+2}</b>
+//             </div>
+//             <button>PROCCED TO CHECK</button>
+//           </div>
+//           <div className="cart-promocode">
+//           <div>
+//             <p>If you have a promo code, Enter it here</p>
+//             <div className="cart-promocode-input">
+//               <input type="text" placeholder="promocode" />
+//               <button>Submit</button>
+//             </div>
+//           </div>
+//         </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Cart;
+
 import React, { useContext } from "react";
-import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
 
   return (
-    <div className="cart">
+    <div className="mt-24">
       <div className="cart-items">
-        <div className="cart-item-title">
+        {/* Cart Headers */}
+        <div className="grid grid-cols-6 items-center text-gray-500 text-sm md:text-base">
           <p>Items</p>
           <p>Title</p>
           <p>Price</p>
@@ -17,54 +92,73 @@ const Cart = () => {
           <p>Remove</p>
         </div>
         <br />
-        <hr />
+        <hr className="h-[1px] bg-gray-200 border-none" />
+        
+        {/* Cart Items */}
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <>
-                <div key={index} className="cart-item-title cart-items-item">
-                  <img src={item.image} alt="" />
+              <React.Fragment key={index}>
+                <div className="grid grid-cols-6 items-center my-2 text-black">
+                  <img src={item.image} alt="" className="w-12" />
                   <p>{item.name}</p>
                   <p>Rs. {item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>Rs. {item.price * cartItems[item._id]}</p>
-                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                  <p
+                    onClick={() => removeFromCart(item._id)}
+                    className="cursor-pointer text-red-500"
+                  >
                     X
                   </p>
                 </div>
-                <hr />
-              </>
+                <hr className="h-[1px] bg-gray-200 border-none" />
+              </React.Fragment>
             );
           }
+          return null;
         })}
-        <div className="cart-bottom">
-          <div className="cart-total">
-            <h2>Cart Total</h2>
-            <div className="cart-total-details">
+
+        {/* Cart Footer */}
+        <div className="mt-20 flex flex-col lg:flex-row lg:justify-between lg:gap-20">
+          {/* Total Section */}
+          <div className="flex-1 flex flex-col gap-5">
+            <h2 className="text-lg font-semibold">Cart Total</h2>
+            <div className="flex justify-between text-gray-600">
               <p>SubTotal</p>
               <p>Rs. {getTotalCartAmount()}</p>
             </div>
-            <hr />
-            <div className="cart-total-details">
+            <hr className="h-[1px] bg-gray-200 border-none" />
+            <div className="flex justify-between text-gray-600">
               <p>Delivery Fee</p>
-              <p>Rs. {2}</p>
+              <p>Rs. 2</p>
             </div>
-            <hr />
-            <div className="cart-total-details">
+            <hr className="h-[1px] bg-gray-200 border-none" />
+            <div className="flex justify-between font-semibold">
               <b>Total</b>
-              <b>Rs. {getTotalCartAmount()+2}</b>
+              <b>Rs. {getTotalCartAmount() + 2}</b>
             </div>
-            <button>PROCCED TO CHECK</button>
+            <button className="bg-brown text-white w-full lg:w-1/3 py-3 rounded-md">
+              PROCEED TO CHECK
+            </button>
           </div>
-          <div className="cart-promocode">
-          <div>
-            <p>If you have a promo code, Enter it here</p>
-            <div className="cart-promocode-input">
-              <input type="text" placeholder="promocode" />
-              <button>Submit</button>
+
+          {/* Promo Code Section */}
+          <div className="flex-1">
+            <p className="text-gray-600">
+              If you have a promo code, Enter it here
+            </p>
+            <div className="mt-2 flex justify-between items-center bg-gray-200 rounded-md p-2">
+              <input
+                type="text"
+                placeholder="Promo Code"
+                className="bg-transparent outline-none px-2 flex-1"
+              />
+              <button className="bg-black text-white py-2 px-6 rounded-md">
+                Submit
+              </button>
             </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
