92565f271228ee298241400a9cac65fcd5d2a2b5
VK-Garg7 <14garg04@gmail.com>
2024-11-28 23:51:25 +0530

    server

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+
+
+//app config
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
+app.listen(port,()=>{
+    console.log(`Server started at https://localhost:${port}`);
+})
