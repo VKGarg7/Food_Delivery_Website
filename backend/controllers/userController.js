96a1586072dd3253375427808442cb0e83a46ca7
VK-Garg7 <14garg04@gmail.com>
2024-11-30 00:22:15 +0530

    register user

---
+++ backend/controllers/userController.js
@@ -0,0 +1,59 @@
+import userModel from "../models/usermodels.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+//login user
+const loginUser = async (req, res) => {
+
+}
+
+const createToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET,{
+        // expiresIn:3*24*60*60
+    });
+}
+
+//register user
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        //check if user already exists
+        const exists = await userModel.findOne({ email });
+        if(exists){
+            return res.json({success:false,message:"User already exists"});
+        }
+
+
+        //validate email and strong password
+        if(!validator.isEmail(email)){
+            return res.json({success:false,message:"Invalid email"});
+        }
+
+        if(password.length<8){
+            return res.json({success:false,message:"Password must be atleast 8 characters long"});
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //create user
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        });
+
+        //save user
+        const user = await newUser.save();
+        const token = createToken(user._id);
+        res.json({success:true,token});
+            
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Server error"});
+    }
+}
+
+export {loginUser,registerUser};
