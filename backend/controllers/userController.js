2fd84682215e171768012873caea316e97256d9e
VK-Garg7 <14garg04@gmail.com>
2024-11-30 00:32:42 +0530

    functionality to login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,28 @@ import validator from "validator";
 
 //login user
 const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await userModel.findOne({ email });
 
+        // user not found
+        if(!user){
+            return res.json({success:false,message:"User not found"});
+        }
+
+        //check password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.json({success:false,message:"Invalid credentials"});
+        }
+
+        const token = createToken(user._id);
+        res.json({success:true,token});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Server error"});
+    }
 }
 
 const createToken = (id) => {
