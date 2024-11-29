0e2bd0bcbbb8e418c37027af5cca163fdd42c315
VK-Garg7 <14garg04@gmail.com>
2024-11-29 11:29:29 +0530

    remove items

--- backend/controllers/foodControllers.js
+++ backend/controllers/foodControllers.js
@@ -40,7 +40,16 @@ const listFood = async (req, res) => {
 
 //remove food item
 const removeFood = async(req,res)=>{
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, ()=>{})
 
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success: true, message: "Food item removed successfully"})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Failed to remove food item"})
+    }
 }
 
 export { addFood,listFood,removeFood}
