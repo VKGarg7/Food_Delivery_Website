c82c6b8dc1aa721f3cc7e081667d7993a9528d9f
VK-Garg7 <14garg04@gmail.com>
2024-12-16 00:46:44 +0530

    header image

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -49,12 +49,12 @@ const Header = () => {
   return (
     <div className="relative h-[34vw] my-[30px] bg-cover bg-no-repeat bg-center" style={{ backgroundImage: "url('/header_img.png')" }}>
       <div className="absolute flex flex-col gap-[1.5vw] max-w-[50%] bottom-[10%] left-[6vw] animate-fadeIn md:max-w-[45%] sm:max-w-[65%]">
-        <h2 className="text-white font-medium text-[max(4.5vw,20px)]">Order your favourite food here</h2>
-        <p className="text-white text-[1vw] hidden sm:block">
+        <h2 className="text-black font-medium text-[max(4.5vw,20px)]">Order your favourite food here</h2>
+        <p className="text-black text-[1vw] hidden sm:block">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi officia
           illum, quibusdam earum vitae amet? Pariatur voluptates dolores a fuga!
         </p>
-        <button className="border-none text-[#747474] font-medium py-[0.5vw] px-[1.5vw] bg-white text-[max(0.8vw,12px)] rounded-full sm:py-[1vw] sm:px-[3vw]">
+        <button className="border-none text-[#ffffff] font-medium py-[0.5vw] px-[1.5vw] bg-black text-[max(0.8vw,12px)] rounded-full sm:py-[1vw] sm:px-[3vw]">
           View Menu
         </button>
       </div>
